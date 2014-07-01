24612398cb86ec5a55bd8deec336dae6ba425979
Eben Freeman <eben@inboxapp.com>
2014-07-01 12:47:18 -0700

    Update Ubuntu iso and remove support for everything but VirtualBox for now.

--- build.sh
+++ build.sh
@@ -3,12 +3,7 @@ set -e
 
 echo 'Starting Vagrant VM for docker builds...'
 # See README for why we do this.
-cd docker_vm && vagrant up && vagrant halt && vagrant up && cd ..
-alias docker='docker -H tcp://127.0.0.1:4243/'
-export DOCKER_HOST="tcp://127.0.0.1:4243/"
-
-echo "Building all images..."
+echo "Building image..."
 packer build packer.json
-cd docker_vm && vagranthalt && cd ..
 
 echo "Done!"
